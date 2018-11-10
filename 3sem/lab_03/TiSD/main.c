c72d61b5e823cdfd1c19645d1a03c0b06276dd6d
saa17u754 <dreamking4@gmail.com>
2018-11-11 01:13:45 +0300

    Fixes of 3rd work and otchet.

--- 3sem/lab_03/TiSD/main.c
+++ 3sem/lab_03/TiSD/main.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#define N_RAND 100
+#define N_RAND 10
 
 // Info
 void info(void)
 {
 	puts("This programm can multiply row-vector by matrix\n"
-	"You can fill matrix by yourself or by computer\n"
+	"You can fill matrix by ourself or by computer\n"
 	"Also you can compare two methods of multiplication");
 }
 
@@ -64,10 +64,16 @@ void print_matrix(int **p, int n, int m)
 // Автозаполнение
 void auto_fill(int **matr, int n, int m, int percentage)
 {
-    int zeroes = (n * m) * percentage / 100;
+	int tmp;
+    long int zeroes = ((n * m) * percentage) / 100;
     for (int i = 0; i < n; i ++)
         for (int j = 0; j < m; j ++)
-            matr[i][j] = (rand() % (2*N_RAND) - N_RAND);
+		{
+			tmp = rand() % (2*N_RAND) - N_RAND;
+			while(!tmp)
+				tmp = rand() % (2*N_RAND) - N_RAND;
+            matr[i][j] = tmp;
+		}
     while (zeroes > 0)
     {
         int k = rand() % n;
@@ -80,16 +86,55 @@ void auto_fill(int **matr, int n, int m, int percentage)
     }
 }
 
+int hand_fill_vect(int **vect, int n)
+{
+	int nz, i, num, flag = 1;
+	printf("Enter the number of nonzero elements for vector (<= %d): ", n);
+	if (scanf("%d",&nz) != 1 || nz > n || nz < 0)
+		return -1;
+	for (int i = 0; i < n; i ++)
+		vect[0][i] = 0;
+	puts("Enter index of nonzero element and element for vector");
+	while (flag)
+	{
+		printf("#%d [i] (<%d), element: ", nz, n);
+		if (scanf("%d %d", &i, &num) != 2 || i >= n || i < 0 || vect[0][i] != 0 || num == 0)
+		{
+			puts("NO");
+			continue;
+		}
+		vect[0][i] = num;
+		nz --;
+		if (nz == 0)
+			flag = 0;
+	}
+	return 0;
+}
+
 // Ручное заполнение
 int hand_fill(int **matr, int n, int m)
 {
-    for (int i = 0; i < n; i ++)
-        for (int j = 0; j < m; j ++)
-        {
-            printf("Matr[%d][%d] = ", i, j);
-            if (scanf("%d", &matr[i][j]) != 1)
-                return -1;
-        }
+	int nz = 0, i, j, num, flag = 1;
+	printf("Enter the number of nonzero elements(<=%d): ",n*m);
+	if (scanf("%d",&nz) != 1 || nz > n*m || nz < 0)
+		return -1;
+	for (int i = 0; i < n; i ++)
+		for (int j = 0; j < m; j++)
+			matr[i][j] = 0;
+	puts("Enter row and column of nonzero elements and elements for matrix");
+	while (flag)
+	{
+		printf("#%d [i] (<%d), [j](<%d), element: ",nz, n, m);
+		if (scanf("%d %d %d", &i, &j, &num) != 3 || i >= n || j >= m || i < 0 || j < 0 || matr[i][j] != 0 || num == 0)
+		{
+			puts("NO");
+			continue;
+		}
+		matr[i][j] = num;
+		nz --;
+		if (nz == 0)
+			flag = 0;
+	}
     return 0;
 }
 
@@ -187,7 +232,7 @@ int main(void)
     srand (time(NULL));
 	info();
     int n, m, m1, k, l;
-	long int t1, t2, s1_time = 0;
+	long int t1, t2, k_sum = 0, s1_time = 0, s2_time = 0, p_sum = 0, mem1 = 0, mem2 = 0;
 	setbuf(stdout, NULL);
 	int percentage;
 	printf("\nLength of vector: ");
@@ -214,9 +259,9 @@ int main(void)
 	int *res = malloc(m * sizeof(int));
     if (matr && vect)
     {
-		puts("Menu:\n"
-		"1 - fill by hand\n"
-		"2 - fill auto\n"
+		printf("Menu:\n"
+		"1 - multiplication, fill with hand\n"
+		"2 - multiplication, fill auto\n"
 		"3 - time\n"
 		"Option: ");
 		if (scanf("%d",&l) != 1)
@@ -227,26 +272,25 @@ int main(void)
 		switch(l)
 		{
 			case 1:
-				if (hand_fill(vect, 1, m1) == 0)
+				printf("\nVector\n");
+				if (hand_fill_vect(vect, m1) != 0)
+				{
+					puts("Wrong input");
+					return -1;
+				}
+				puts("\nMatrix");
+				if (hand_fill(matr, n, m) != 0)
+				{
+					puts("Wrong input");
+					return -1;
+				}
+				if (m1 <= 10)
 				{
 					puts("Vector");
 					print_matrix(vect, 1, m1);
-				}
-				else
-					{
-						puts("Wrong input");
-						return -1;
-					}
-				if (hand_fill(matr, n, m) == 0)
-				{
 					puts("Matrix");
 					print_matrix(matr, n, m);
 				}
-				else
-					{
-						puts("Wrong input");
-						return -1;
-					}
 				partition(matr, n, m, A, IA, JA, &k);
 				printf(" A: ");
 				for (int i = 0; i < k; i++)
@@ -260,11 +304,13 @@ int main(void)
 				for (int i = 0; i < m+1; i++)
 					printf("%d ",JA[i]);
 				multiply_matrix(JA, IA, A, m, vect, res);
+				partition_vect(res, m, A1, IA1, JA1, &k);
+				puts("\n\nResult");
 				if (m1 < 10)
 					for (int i = 0; i < m; i ++)
 						printf("%d ", res[i]);
 				puts("");
-				partition_vect(res, m, A1, IA1, JA1, &k);
+				puts("");
 				printf(" A: ");
 				for (int i = 0; i < k; i++)
 					printf("%d ",A1[i]);
@@ -276,22 +322,24 @@ int main(void)
 				printf("JA: ");
 				for (int i = 0; i < m+1; i++)
 					printf("%d ",JA1[i]);
-				puts("\nResult");
 				puts("");
 				break;
 			case 2:
-				printf("Percentage: ");
-				if (scanf("%d", &percentage) != 1)
-					{
-						puts("Wrong input");
-						return -1;
-					}
+				printf("Percentage of sparsity: ");
+				if (scanf("%d", &percentage) != 1 || percentage > 100 || percentage < 0)
+				{
+					puts("Wrong input");
+					return -1;
+				}
 				auto_fill(matr, n, m, percentage);
 				auto_fill(vect, 1, m1, percentage);
-				puts("Vector");
-				print_matrix(vect, 1, m1);
-				puts("Matrix");
-				print_matrix(matr, n, m);
+				if (m1 <= 10)
+				{
+					puts("Vector");
+					print_matrix(vect, 1, m1);
+					puts("Matrix");
+					print_matrix(matr, n, m);
+				}
 				partition(matr, n, m, A, IA, JA, &k);
 				printf(" A: ");
 				for (int i = 0; i < k; i++)
@@ -308,9 +356,10 @@ int main(void)
 				partition_vect(res, m, A1, IA1, JA1, &k);
 				puts("\n\nResult");
 				puts("");
-				if (m1 < 10)
+				if (m1 <= 10)
 					for (int i = 0; i < m; i ++)
 						printf("%d ", res[i]);
+				puts("");
 				puts("");
 				printf(" A: ");
 				for (int i = 0; i < k; i++)
@@ -326,13 +375,14 @@ int main(void)
 				break;
 				
 			case 3:
-				printf("Percentage: ");
-				if (scanf("%d", &percentage) != 1)
+				printf("Percentage of sparsity: ");
+				if (scanf("%d", &percentage) != 1 || percentage > 100 || percentage < 0)
 					{
 						puts("Wrong input");
 						return -1;
 					}
-				for (int i = 0; i < 1; i ++)
+				s1_time = 0;
+				for (int i = 0; i < 50; i ++)
 				{
 					auto_fill(matr, n, m, percentage);
 					auto_fill(vect, 1, m1, percentage);
@@ -340,19 +390,41 @@ int main(void)
 					t1 = tick();
 					multiply_matrix(JA, IA, A, m, vect, res);
 					t2 = tick();
+					k_sum += k;
+					for (int i = 0; i < m; i ++)
+						if (res[i])
+							p_sum ++;
 					s1_time += (t2 - t1);
 				}
-				printf("Time: %ld\n", (long int)(s1_time/50));
-				for (int i = 0; i < 1; i ++)
+				printf("\nMultiplication time (special method): %ld\n", (long int)(s1_time/50));
+				s1_time = s1_time/50;
+				mem1 = (long)((sizeof(int)) * (2 * (k_sum/50) + (m+1) + \
+				2 * (p_sum/50) + (m+1) + k_sum/50 * 3 + (m+1)));
+				printf("Memory: %ld\n", mem1);
+				for (int i = 0; i < 50; i ++)
 				{
-					auto_fill(matr, n, m, percentage);
-					auto_fill(vect, 1, m1, percentage);
+					auto_fill(matr, n, m, percentage-1);
+					auto_fill(vect, 1, m1, percentage-1);
 					t1 = tick();
 					multiplication(vect, matr, res, 1, m1, m);
 					t2 = tick();
-					s1_time += (t2 - t1);
+					s2_time += (t2 - t1);
 				}
-				printf("Time: %ld\n", (long int)(s1_time/50));
+				printf("\nMultiplication time (standart method): %ld\n", (long int)(s2_time/50));
+				s2_time = s2_time/50;
+				mem2 = (long)(sizeof(int) * ((m1 * n) + m + m1));
+				printf("Memory: %ld\n", mem2);
+				
+				if (s1_time > s2_time)
+					printf("\nStandart method is faster than special method in %4.3lf percent\n", ((double)(s1_time - s2_time) / s1_time) * 100);
+				else
+					printf("Special method is faster than standart method in %4.3lf percent\n", ((double)(s2_time - s1_time) / s2_time) * 100);
+				
+				if (mem1 > mem2)
+					printf("Standart method takes %4.3lf percent less memory than special method\n", ((double)(mem1 - mem2) / mem1) * 100);
+				else
+					printf("Special method takes %4.3lf percent less memory than standart method\n", ((double)(mem2 - mem1) / mem2) * 100);
+				
 				break;
 		}
     }

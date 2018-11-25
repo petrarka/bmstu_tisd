54ee081b4e3055b9bd6690e3d9982437fa0199c9
saa17u754 <dreamking4@gmail.com>
2018-11-25 15:18:21 +0300

    Lab04 complete!! Yaay!

--- 3sem/lab_04/Try1/main.c
+++ 3sem/lab_04/Try1/main.c
@@ -1,39 +1,132 @@
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
-#define MAX_LEN 1
+#define MAX_LEN 20
 typedef struct node Stack;
 
 struct node
 {
     char data;
-    Stack *next;
+    Stack *prev;
 };
 
-/*
-void clear (Stack *head)
+void clear (Stack *list)
 {
-    Stack *next;
-    for ( ; head; head = next)
+    while (list)
     {
-        next = head->next;
-        free(head);
+        Stack *element = list->prev;
+        free(list);
+        list = element;
     }
 }
 
-Stack* create_new_element (char data)
+Stack* push_list (Stack **list, void **adr, int *pushed)
 {
-    Stack *tmp = malloc(sizeof(Stack));
-    if (tmp)
+    char data;
+    printf("Enter characer: ");
+    scanf(" %c",&data);
+    getchar();
+    Stack *element = malloc(sizeof(Stack));
+    adr[*pushed] = (void *)element;
+    *pushed += 1;
+    if (element)
     {
-        tmp -> data = data;
-        tmp -> next = NULL;
-        return tmp;
+        element->data = data;
+        if (list)
+        {
+            element->prev = *list;
+            *list = element;
+        }
+        else
+            element->prev = NULL;
+    }
+    return element;
+}
+
+int pop(Stack **list, char *data, void **adr, int *poped)
+{
+    if (*list == NULL)
+    {
+        puts("\nStack is empty!\n");
+        return -1;
+    }
+    *data = (*list)->data;
+    Stack *element = *list;
+    *list = (*list)->prev;
+    adr[*poped] = (void *)element;
+    *poped += 1;
+    //printf("%p\n",(void *)element);
+    free(element);
+    return 0;
+}
+
+void print_list(Stack *head)
+{
+    if (head == NULL)
+    {
+        puts("Stack is empty!\n");
+        return;
+    }
+    printf("STACK ENDS HERE |");
+    for ( ; head; head = head->prev)
+    {
+        printf(" %c |", head->data);
+    }
+    puts("\n");
+}
+
+int process_list(Stack **head, void **adr, int *poped)
+{
+    if (*head == NULL)
+    {
+        puts("Stack is empty!\n");
+        return -1;
+    }
+    char element;
+    char parenth_stack[MAX_LEN];
+    int i = -1;
+    //int flag = 2;
+    while (*head)
+    {
+        if (pop(head, &element, adr, poped) != 0)
+        {
+            puts("ERR");
+            return -1;
+        }
+        if (element == ')' || element == ']' || element == '}')
+        {
+            i++;
+            parenth_stack[i] = element;
+        }
+        else if (element == '(' || element == '[' || element == '{')
+        {
+            if (i == -1 || (element == '(' && parenth_stack[i] != ')') ||
+        (element == '[' && parenth_stack[i] != ']') || (element == '{' && parenth_stack[i] != '}'))
+            {
+                puts("\nParentheses are not balanced!");
+                return -1;
+            }
+            else
+                i --;
+        }
+        //printf("%c ",element);
+        //flag --;
+    }
+    if (i == -1)
+    {
+        puts("\nParentheses are balanced!");
+        return 0;
     }
     else
-        return NULL;
+    {
+        puts("\nParentheses are not balanced!");
+        return -1;
+    }
+    puts("");
+    return 0;
 }
 
+/*
 Stack* add_front (Stack *head, Stack *element)
 {
     element->next = head;
@@ -117,172 +210,203 @@ int input_arr (char *stack, char **head)
     }
     return 0;
 }
-
-void print_list(Stack *head)
-{
-    puts("");
-    for ( ; head; head = head->next)
-    {
-        printf("%c ", head->data);
-    }
-    puts("");
-}
 */
 
-
-int process(char *stack, char *head)
+void print_adr(void **adr, int n)
 {
-    if (head < stack)
+    if (n == 0)
     {
-        puts("Stack is empty");
-        return -1;
+        puts("EMPTY\n");
+        return ;
     }
-    int cir_op = 0, cir_cl = 0;
-    int rec_op = 0, rec_cl = 0;
-    int fig_op = 0, fig_cl = 0;
-    while (stack < head--)
-    {
-        if (*(head) == '(')
-        {
-            cir_op ++;
-        }
-        else if (*(head) == ')')
-        {
-            cir_cl ++;
-        }
-        if (cir_op > cir_cl)
-        {
-            puts("EnD");
-            return -1;
-        }
-
-        if (*(head) == '{')
-        {
-            fig_op ++;
-        }
-        else if (*(head) == '}')
-        {
-            fig_cl ++;
-        }
-        if (fig_op > fig_cl)
-        {
-            puts("EnD");
-            return -1;
-        }
-
-        if (*(head) == '[')
-        {
-            rec_op ++;
-        }
-        else if (*(head) == ']')
-        {
-            rec_cl ++;
-        }
-        if (rec_op > rec_cl)
-        {
-            puts("EnD");
-            return -1;
-        }
-
-        if (cir_op != 0 && (rec_op - cir_op > 1 || fig_op > cir_op))
-        {
-            puts("ENDO");
-            return -1;
-        }
-        else if (rec_op != 0 && (cir_op - rec_op > 1 || fig_op > rec_op))
-        {
-            puts("ENDP");
-            return -1;
-        }
-        else if (fig_op != 0 && (cir_op - fig_op > 1 || rec_op > fig_op))
-        {
-            puts("EDNSD");
-            return -1;
-        }
-    }
-    if (rec_cl != rec_op)
-    {
-        puts("End");
-        return -1;
-    }
-    if (fig_cl != fig_op)
-    {
-        puts("End");
-        return -1;
-    }
-    if (cir_cl != cir_op)
-    {
-        puts("End");
-        return -1;
-    }
-    printf("%d %d\n",fig_op, fig_cl);
-    printf("%d %d\n",rec_op, rec_cl);
-    printf("%d %d\n",cir_op, cir_cl);
-    return 0;
+    for (int i = 0; i < n; i ++)
+        printf("%p\n", adr[i]);
+    puts("");
 }
 
 void print_arr(char *begin, char *cur)
 {
-    while (cur >= begin)
+    if (cur < begin)
     {
-        printf("%c", *(cur-1));
-        cur --;
+        puts("Stack is empty!!\n");
+        return ;
     }
-    puts("_");
+    printf("|");
+    while (begin <= cur)
+    {
+        printf(" %c |", *begin);
+        begin ++;
+    }
+    puts(" STACK ENDS HERE\n");
 }
 
 int push_arr(char *begin, char *end, char **cur)
 {
     char el;
     printf("Enter characer: ");
-    el = fgetc(stdin);
+    scanf(" %c",&el);
+    getchar();
     *cur += 1;
     if (*cur + 1 > end)
     {
-        *cur = end;
-        puts("Stack is full");
+        *cur = end - 1;
+        puts("\nStack is full!!\n");
         return -1;
     }
     **cur = el;
-    gets(&el);
+    //printf("_%c_",**cur);
     return 0;
 }
 
-int pop_arr(char *begin, char *end, char **cur)
+int pop_arr(char *begin, char *element, char **cur)
 {
-    if (*cur < begin)
+    *element = **cur;
+    *cur -= 1;
+    if (*cur + 1 < begin)
+    {
+        *cur = begin - 1;
+        puts("\nStack is empty!!\n");
+        return -1;
+    }
+    return 0;
+}
+
+int process(char *stack, char **head)
+{
+    if (*head < stack)
     {
         puts("Stack is empty");
         return -1;
     }
-    *cur -= 1;
-    return 0;
+
+    char parenth_stack[MAX_LEN];
+    char element;
+    int i = -1;
+    while (stack <= *head)
+    {
+        if (pop_arr(stack, &element, head) != 0)
+            return -1;
+        if (element == ')' || element == ']' || element == '}')
+        {
+            i++;
+            parenth_stack[i] = element;
+        }
+        else if (element == '(' || element == '[' || element == '{')
+        {
+            if (i == -1 || (element == '(' && parenth_stack[i] != ')') ||
+        (element == '[' && parenth_stack[i] != ']') || (element == '{' && parenth_stack[i] != '}'))
+            {
+                puts("\nParentheses are not balanced!");
+                return -1;
+            }
+            else
+                i --;
+        }
+        //head --;
+    }
+    if (i == -1)
+    {
+        puts("\nParentheses are balanced!");
+        return 0;
+    }
+    else
+    {
+        puts("\nParentheses are not balanced!");
+        return -1;
+    }
 }
 
 int main (void)
 {
+    void *pushed_adr[MAX_LEN];
+    void *poped_adr[MAX_LEN];
+    int pushed = 0, poped = 0;
     setbuf(stdout, NULL);
     char stack_arr[MAX_LEN];
     char *arr_head = stack_arr;
     char *cur = arr_head - 1;
     char *arr_end = stack_arr + MAX_LEN;
-    printf("beg: %p \nend: %p \ncur: %p ___ %d\n", arr_head, arr_end, cur, (int)(arr_end - arr_head));
-    //print_arr(arr_head, cur);
-    push_arr(arr_head, arr_end, &cur);
-    printf("beg: %p \nend: %p \ncur: %p ___ %d\n", arr_head, arr_end, cur, (int)(arr_end - arr_head));
-    push_arr(arr_head, arr_end, &cur);
-    printf("beg: %p \nend: %p \ncur: %p ___ %d\n", arr_head, arr_end, cur, (int)(arr_end - arr_head));
-    print_arr(arr_head, arr_end);
-    puts("");
-    //process(arr_head, cur);
-    /*
-    input_arr(stack_arr, &arr_head);
-    print_arr(stack_arr, arr_head);
-    process(stack_arr, arr_head);
-    push_arr(stack_arr, &arr_head, 's');
-    push_arr(stack_arr, &arr_head, 'o');
-    push_arr(stack_arr, &arr_head, 'e');
-    print_arr(stack_arr, arr_head);
-    pop_arr(stack_arr, &arr_head);*/
+    int flag = 1;
+    int option1, option;
+    char element;
+    Stack *head = NULL;
+    printf("How to realize your stack?\n"
+        "1 - array\n"
+        "2 - list\n"
+        "Option: ");
+    if (scanf("%d", &option1) != 1)
+        return -1;
+    if (option1 == 1)
+    {
+        while (flag)
+        {
+            puts("__________________________________________");
+            puts("Stack: ");
+            print_arr(arr_head, cur);
+            printf("1 - push element\n"
+            "2 - pop element\n"
+            "3 - check parentheses\n"
+            "0 - exit\n"
+            "Option: ");
+            if (scanf("%d", &option) != 1)
+            {
+                puts("Wrong input");
+                return -1;
+            }
+            if (option == 1)
+            {
+                if (push_arr(arr_head, arr_end, &cur) == 0)
+                    puts("Pushed!\n");
+            }
+            else if (option == 2)
+            {
+                if (pop_arr(arr_head, &element, &cur) == 0)
+                    printf("\nElement: %c\n", element);
+            }
+            else if (option == 3)
+                process(arr_head, &cur);
+            else if (option == 0)
+                flag = 0;
+        }
+    }
+    else if (option1 == 2)
+    {
+        while (flag)
+        {
+            puts("__________________________________________");
+            puts("Stack: ");
+            print_list(head);
+            puts("Pushed adresses: ");
+            print_adr(pushed_adr, pushed);
+            puts("Poped adresses: ");
+            print_adr(poped_adr, poped);
+            printf("1 - push element\n"
+            "2 - pop element\n"
+            "3 - check parentheses\n"
+            "0 - exit\n"
+            "Option: ");
+            if (scanf("%d", &option) != 1)
+            {
+                puts("Wrong input");
+                return -1;
+            }
+            if (option == 1)
+            {
+                push_list(&head, pushed_adr, &pushed);
+            }
+            else if (option == 2)
+                pop(&head, &element, poped_adr, &poped);
+            else if (option == 3)
+            {
+                process_list(&head, poped_adr, &poped);
+                //flag = 0;
+            }
+            else if (option == 0)
+            {
+                flag = 0;
+                clear(head);
+            }
+        }
+    }
     return 0;
 }
